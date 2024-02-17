f12e8036ebf67badf29a793332943a0987dedb36
香月大我 <>
2024-02-17 19:52:14 +0900

    no message

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    /*
+    public Vector3 Start { get; set; }
+    public Vector3 Direct { get; set; }
+    public float Speed;
+
+    void Init(Vector3 ){
+
+    }
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
